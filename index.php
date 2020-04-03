ee7ecd64c27219a6d3b7e95d21f26913ac9fa1f7
MedinaVilla <net_medina@hotmail.com>
2020-04-02 22:17:01 -0600

    Avances modulo administrador. Reportes

--- index.php
+++ index.php
@@ -16,11 +16,17 @@
         case "/EIIS-ESCOM/alumnos" :
             require __DIR__ . '/src/administrador/panel/alumnos/alumnos.html';
             break;
-        case "/EIIS-ESCOM/reportes" :
-            require __DIR__ . '/src/administrador/panel/reportes/reportes.html';
-            break;
         case "/EIIS-ESCOM/materias" :
             require __DIR__ . '/src/alumno/materias/selectMaterias.html';
+            break;
+        case "/EIIS-ESCOM/reporte_alumno" :
+            require __DIR__ . '/src/administrador/panel/reportes/alumno/alumno.html';
+            break;
+        case "/EIIS-ESCOM/reporte_materia" :
+            require __DIR__ . '/src/administrador/panel/reportes/materia/materia.html';
+            break;
+        case "/EIIS-ESCOM/cerrarsesion" :
+            require __DIR__ . '/src/administrador/session/logout.php';
             break;
         default:
             http_response_code(404);
