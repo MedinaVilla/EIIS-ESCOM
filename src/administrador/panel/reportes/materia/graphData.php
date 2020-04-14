891cbb1f613cc9cf6107b0e952b64439c5f51a32
MedinaVilla <net_medina@hotmail.com>
2020-04-14 14:28:38 -0500

    Mejora de los modulos alumnos y reporte alumno

--- src/administrador/panel/reportes/materia/graphData.php
+++ src/administrador/panel/reportes/materia/graphData.php
@@ -4,7 +4,7 @@
         require_once('./../../../../../config/mysqli_connect.php');
         
         $sql = "SELECT F4.materia,F2.inscritos_regular,F4.inscritos_recurse FROM (SELECT F1.materia,count(situacion.situacion)AS inscritos_regular FROM (SELECT asignatura.materia,asignatura_intencion.situacion_idsituacion FROM asignatura 
-        INNER JOIN asignatura_intencion ON asignatura.idmateria=asignatura_intencion.asignatura_idmateria where asignatura.materia='Cálculo')AS F1 inner join situacion on F1.situacion_idsituacion=situacion.idsituacion 
+        INNER JOIN asignatura_intencion ON asignatura.idmateria=asignatura_intencion.asignatura_idmateria where asignatura.materia='".$materia."')AS F1 inner join situacion on F1.situacion_idsituacion=situacion.idsituacion 
         where situacion.situacion='regular')AS F2,(SELECT F3.materia,count(situacion.situacion)AS inscritos_recurse FROM (SELECT asignatura.materia,asignatura_intencion.situacion_idsituacion FROM asignatura 
         INNER JOIN asignatura_intencion ON asignatura.idmateria=asignatura_intencion.asignatura_idmateria where asignatura.materia='".$materia."')AS F3 inner join situacion on F3.situacion_idsituacion=situacion.idsituacion where situacion.situacion='recurse')AS F4;";
         $result = mysqli_query($conn,$sql);
