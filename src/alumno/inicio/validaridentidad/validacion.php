d7ce6b841e686f71e29cce85be21995552cf34d7
jaimechacon11 <chacon.jaime99g@gmail.com>
2020-04-18 21:26:53 -0500

    verificacioncompleta

--- src/alumno/inicio/validaridentidad/validacion.php
+++ src/alumno/inicio/validaridentidad/validacion.php
@@ -6,8 +6,19 @@ require_once('./../../../../config/mysqli_connect.php');
     $result = mysqli_query($conn,$sql);
     $resultCheck = mysqli_num_rows($result);
     if($resultCheck>0){
-        if ($row = mysqli_fetch_array($result)) {
-            echo $row['boleta'];
+        $verificacion = 1;
+        $sql2 = "select activacion from alumno where boleta='".$boleta."' AND activacion = '".$verificacion."';";
+        $result2 = mysqli_query($conn,$sql2);
+        $resultCheck2 = mysqli_num_rows($result2);
+        if($resultCheck2==0){
+            if ($row = mysqli_fetch_array($result)) {
+                //echo $row['boleta'];
+                echo 1;
+            }
+        }else {
+            echo 2;   
         }
-    } 
+    } else{
+        echo 3;
+    }
 ?>
