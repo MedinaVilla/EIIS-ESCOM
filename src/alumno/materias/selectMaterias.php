579d4a3ea0203f8d50f6dbc39eee2ca86de46055
Scormit <sbenitezr@outlook.com>
2020-04-24 18:18:54 -0500

    Inserto Alumno de Intencion a la BD

--- src/alumno/materias/selectMaterias.php
+++ src/alumno/materias/selectMaterias.php
@@ -9,20 +9,54 @@
         $result = mysqli_query($conn,$sql);
         $resultCheck = mysqli_num_rows($result);
         if($resultCheck==0){
+            $sql = "select count(*) from intencion;";
+            $result = mysqli_query($conn,$sql);
+            $resultCheck = mysqli_num_rows($result);
+            if($resultCheck>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $idIntencion = $row['count(*)'] + 1;
+                }
+                date_default_timezone_set('America/Mexico_City');
+                setlocale(LC_TIME, 'es_MX.UTF-8');
+                $fec_inten=date("Y-m-d");
+                $sql = "INSERT INTO intencion (idintencion, alumno_boleta, fecha_intencion) values ('$idIntencion', '$boleta', '$fec_inten');";
+                mysqli_query($conn, $sql);
+                
+                /*
+                foreach($curses as $key => $value){
+                    $sql = "select * from asignatura where materia='".$value."';";
+                    $result = mysqli_query($conn,$sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if($resultCheck>0){
+                        while($row = mysqli_fetch_assoc($result)){
+                            $idMat = $row['materia'];
+                        }
+                        $sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '1');";
+                        mysqli_query($conn, $sql);
+                    }
+                }
+
+                foreach($recurses as $key => $value){
+                    $sql = "select * from asignatura where materia='".$value."';";
+                    $result = mysqli_query($conn,$sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if($resultCheck>0){
+                        while($row = mysqli_fetch_assoc($result)){
+                            $idMat = $row['materia'];
+                        }
+                        $sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '2');";
+                        mysqli_query($conn, $sql);
+                    }
+                }*/
+            }
+            echo "<p>Tu id es: $idIntencion</p>";
+            //echo "<p>Tu idMat: $idMat</p>";
+            echo "<p>La fecha es: $fec_inten</p>";
             echo "<p> No esta registrado</p>";
         }
+
         else{
             echo "<p> Ya esta registrado</p>";
         }
-
-        /*
-        echo "<h3>Seleccionaste los siguentes curses:</h3><br>";
-        foreach($curses as $key => $value){
-            echo "$value<br>";
-        }
-        echo "<h3>Seleccionaste los siguentes recurses:</h3><br>";
-        foreach($recurses as $key => $value){
-            echo "$value<br>";
-        }*/
     }
 ?>
