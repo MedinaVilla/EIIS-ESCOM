67c34c59c071fbb55110ac6031944fd04537bcc9
MedinaVilla <net_medina@hotmail.com>
2020-05-21 15:16:20 -0500

    merge

--- config/mysqli_connect.php
+++ config/mysqli_connect.php
@@ -2,13 +2,8 @@
   //Database connection variables
   $servername = "localhost";
   $username = "root";
-<<<<<<< HEAD
   $password = "N0M3L0";
-  $dbname  = "eiis";
-=======
-  $password = "";
-  $dbname  = "registro";
->>>>>>> 7df7eeb115779d1049ef6c10ed0b3e93148d9ae4
+  $dbname  = "ingreso";
 
   // Create connection
   $conn= mysqli_connect($servername, $username, $password, $dbname);
