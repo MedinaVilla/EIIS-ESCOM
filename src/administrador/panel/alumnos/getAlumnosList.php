e5d7e11845526a9516127c9b5bb65986dd9e1294
MedinaVilla <net_medina@hotmail.com>
2020-06-06 12:00:58 -0500

    prueba 6

--- src/administrador/panel/alumnos/getAlumnosList.php
+++ src/administrador/panel/alumnos/getAlumnosList.php
@@ -5,13 +5,10 @@
     $sql = "select * from alumno;";
     $result = mysqli_query($conn,$sql);
     $resultCheck = mysqli_num_rows($result);
-    printf("El resultado tiene %d filas.\n", $resultCheck);
     if($resultCheck>0){
         while($row = mysqli_fetch_assoc($result)){
-            printf("Entra a while");
             $alumnos[] = $row;
         }
-        printf($alumnos);
         echo json_encode($alumnos);
     } 
     
