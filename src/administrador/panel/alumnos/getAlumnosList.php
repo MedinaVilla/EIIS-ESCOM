e683c032506a2d3a398ecf2df18f30a6122b9f43
MedinaVilla <net_medina@hotmail.com>
2020-06-06 11:51:45 -0500

    prueba 2

--- src/administrador/panel/alumnos/getAlumnosList.php
+++ src/administrador/panel/alumnos/getAlumnosList.php
@@ -1,9 +1,10 @@
 <?php 
     require_once('./../../../../config/mysqli_connect.php');
     
-    $sql = "select * from alumno;";
+    $sql = "select * from alumno";
     $result = mysqli_query($conn,$sql);
     $resultCheck = mysqli_num_rows($result);
+    printf("El resultado tiene %d filas.\n", $resultCheck);
     $alumnos = [];
     if($resultCheck>0){
         while($row = mysqli_fetch_assoc($result)){
