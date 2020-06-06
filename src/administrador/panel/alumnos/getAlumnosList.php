3c33363aa1b0688bbcb1fcac3494649169a28680
MedinaVilla <net_medina@hotmail.com>
2020-06-06 12:34:37 -0500

    prueba 15

--- src/administrador/panel/alumnos/getAlumnosList.php
+++ src/administrador/panel/alumnos/getAlumnosList.php
@@ -1,8 +1,10 @@
 <?php 
+    header("Content-Type: text/html; charset=utf-8");
     require_once('./../../../../config/mysqli_connect.php');
     
     $alumnos = [];
     $sql = "select * from alumno;";
+    mysql_set_charset('utf8', $conn);
     $result = mysqli_query($conn,$sql);
     $resultCheck = mysqli_num_rows($result);
     if($resultCheck>0){
