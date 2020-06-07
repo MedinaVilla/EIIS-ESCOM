3082069c1426c151942504dc1643381cef16ef0e
Scormit <sbenitezr@outlook.com>
2020-06-07 16:49:10 -0500

    Update13

--- assets/js/UpdateAlumno.js
+++ assets/js/UpdateAlumno.js
@@ -24,7 +24,7 @@ function getDataAlumno() {
             success: function (response) {
                 console.log("voy a ver que me regresa");
                 console.log(response);
-                alumno = JSON.parse(response);
+                alumno = response;
             },
             error: function () {
                 alert('There was some error performing the AJAX call!');
