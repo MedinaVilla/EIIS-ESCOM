8fca43cfb873d439969c645b60bec20cccab6a10
Scormit <sbenitezr@outlook.com>
2020-06-07 17:07:30 -0500

    Update19

--- src/alumno/materias/getDataAlumno.php
+++ src/alumno/materias/getDataAlumno.php
@@ -8,7 +8,7 @@
         $resultCheck = mysqli_num_rows($result);
         if($resultCheck>0){
             while($row = mysqli_fetch_assoc($result)){
-                echo $row['boleta'];
+                echo $row;
             }
         }
         else{
