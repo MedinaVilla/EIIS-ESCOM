1df496c944ab8aa3da72dc8aa48995eaa4da9a99
Scormit <sbenitezr@outlook.com>
2020-06-07 16:46:10 -0500

    Update10

--- src/alumno/materias/getDataAlumno.php
+++ src/alumno/materias/getDataAlumno.php
@@ -2,13 +2,14 @@
     session_start();
     $boleta = $_SESSION['user'];
     require_once('./../../../config/mysqli_connect.php');
-    $alumno;
+    $alumno = [];
     $sql = "select * from alumno where boleta='".$boleta."';";
     $result = mysqli_query($conn,$sql);
     $resultCheck = mysqli_num_rows($result);
     if($resultCheck>0){
         while($row = mysqli_fetch_assoc($result)){
-            echo json_encode($alumno);
+            $alumno[] = $row;
         }
+        echo json_encode($alumnos);
     } 
 ?>
