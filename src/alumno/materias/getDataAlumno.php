25ed9ad60e260395874766520a900b8b2661d9b4
Scormit <sbenitezr@outlook.com>
2020-06-07 17:12:07 -0500

    Update21

--- src/alumno/materias/getDataAlumno.php
+++ src/alumno/materias/getDataAlumno.php
@@ -8,7 +8,7 @@
         $resultCheck = mysqli_num_rows($result);
         if($resultCheck>0){
             while($row = mysqli_fetch_assoc($result)){
-                echo json_encode($row['boleta']);
+                echo json_encode($row);
             }
         }
         else{
