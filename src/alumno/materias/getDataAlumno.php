e6d31cfd1191271de0eaaaaf748df7bf18ba7bd1
Scormit <sbenitezr@outlook.com>
2020-06-07 17:06:31 -0500

    Update18

--- src/alumno/materias/getDataAlumno.php
+++ src/alumno/materias/getDataAlumno.php
@@ -1,18 +1,18 @@
-<?php
+<?php 
     session_start();
-    $boleta = $_SESSION['user'];
-    require_once('./../../../config/mysqli_connect.php');
-    $alumno;
-    $sql = "select * from alumno where boleta='".$boleta."';";
-    $result = mysqli_query($conn,$sql);
-    $resultCheck = mysqli_num_rows($result);
-    if($resultCheck>0){
-        /*while($row = mysqli_fetch_assoc($result)){
-            $alumno = $row;
-        }*/
-        echo $row;
-    } 
-    else{
-        echo "No la encontre";
+    if(isset($_SESSION['user'])){
+        require_once('./../../../config/mysqli_connect.php');
+        $boleta = $_SESSION['user'];
+        $sql = "select * from alumno where boleta='".$boleta."';";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck>0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['boleta'];
+            }
+        }
+        else{
+            echo "No hay nada";
+        }
     }
 ?>
