4ea6ceee995fe3fdad968714de67f44ecc614f40
jaimechacon11 <chacon.jaime99g@gmail.com>
2020-06-08 10:43:29 -0500

    pdf3

--- src/alumno/materias/selectMaterias.php
+++ src/alumno/materias/selectMaterias.php
@@ -1,67 +1,77 @@
 <?php
-session_start();
-if(isset($_POST)){
-$boleta = $_SESSION['user'];
-if(!empty($_POST['curses'])){
-$curses = $_POST['curses'];
-}
-if(!empty($_POST['recurses'])){
-$recurses = $_POST['recurses'];
-}
-require_once('./../../../config/mysqli_connect.php');
-$sql = "select fecha_intencion from intencion where alumno_boleta='".$boleta."';";
-$result = mysqli_query($conn,$sql);
-$resultCheck = mysqli_num_rows($result);
-if($resultCheck==0){
-$sql = "select count(*) from intencion;";
-$result = mysqli_query($conn,$sql);
-$resultCheck = mysqli_num_rows($result);
-if($resultCheck>0){
-while($row = mysqli_fetch_assoc($result)){
-$idIntencion = $row['count(*)'] + 1;
-}
-date_default_timezone_set('America/Mexico_City');
-setlocale(LC_TIME, 'es_MX.UTF-8');
-$fec_inten=date("Y-m-d");
-$sql = "INSERT INTO intencion (idintencion, alumno_boleta, fecha_intencion) values ('$idIntencion', '$boleta', '$fec_inten');";
-mysqli_query($conn, $sql);
+    session_start();
+    if(isset($_POST)){
+        $boleta = $_SESSION['user'];
+        if(!empty($_POST['curses'])){
+            $curses = $_POST['curses'];
+        }
+        if(!empty($_POST['recurses'])){
+            $recurses = $_POST['recurses'];
+        }
+        require_once('./../../../config/mysqli_connect.php');
+        $sql = "select fecha_intencion from intencion where alumno_boleta='".$boleta."';";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck==0){
+            $sql = "select count(*) from intencion;";
+            $result = mysqli_query($conn,$sql);
+            $resultCheck = mysqli_num_rows($result);
+            if($resultCheck>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $idIntencion = $row['count(*)'] + 1;
+                }
+                date_default_timezone_set('America/Mexico_City');
+                setlocale(LC_TIME, 'es_MX.UTF-8');
+                $fec_inten=date("Y-m-d");
+                $sql = "INSERT INTO intencion (idintencion, alumno_boleta, fecha_intencion) values ('$idIntencion', '$boleta', '$fec_inten');";
+                mysqli_query($conn, $sql);
+                
+                if(!empty($curses))
+                {
+                    foreach($curses as $key => $value){
+                        $sql = "select * from asignatura where materia='".$value."';";
+                        $result = mysqli_query($conn,$sql);
+                        $resultCheck = mysqli_num_rows($result);
+                        if($resultCheck>0){
+                            while($row = mysqli_fetch_assoc($result)){
+                                $idMat = $row['idmateria'];
+                            }
+                            $sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '1');";
+                            mysqli_query($conn, $sql);
+                        }
+                    }
+                }
 
-if(!empty($curses))
-{
-foreach($curses as $key => $value){
-$sql = "select * from asignatura where materia='".$value."';";
-$result = mysqli_query($conn,$sql);
-$resultCheck = mysqli_num_rows($result);
-if($resultCheck>0){
-while($row = mysqli_fetch_assoc($result)){
-$idMat = $row['idmateria'];
-}
-$sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '1');";
-mysqli_query($conn, $sql);
-}
-}
-}
-
-if(!empty($recurses))
-{
-foreach($recurses as $key => $value){
-$sql = "select * from asignatura where materia='".$value."';";
-$result = mysqli_query($conn,$sql);
-$resultCheck = mysqli_num_rows($result);
-if($resultCheck>0){
-while($row = mysqli_fetch_assoc($result)){
-$idMat = $row['idmateria'];
-}
-$sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '2');";
-mysqli_query($conn, $sql);
-}
-}
-}
-}
-echo 1;
-}
-else{
-echo 0;
-}
-}
+                if(!empty($recurses))
+                {
+                    foreach($recurses as $key => $value){
+                        $sql = "select * from asignatura where materia='".$value."';";
+                        $result = mysqli_query($conn,$sql);
+                        $resultCheck = mysqli_num_rows($result);
+                        if($resultCheck>0){
+                            while($row = mysqli_fetch_assoc($result)){
+                                $idMat = $row['idmateria'];
+                            }
+                            $sql = "INSERT INTO asignatura_intencion (asignatura_idmateria, intencion_idintencion, situacion_idsituacion) values ('$idMat', '$idIntencion', '2');";
+                            mysqli_query($conn, $sql);
+                        }
+                    }
+                }
+                $sql2 = "SELECT F2.materia,situacion.situacion FROM (SELECT asignatura.materia,F1.situacion_idsituacion FROM (SELECT asignatura_intencion.asignatura_idmateria,asignatura_intencion.situacion_idsituacion FROM asignatura_intencion INNER JOIN intencion ON intencion.idintencion=asignatura_intencion.intencion_idintencion where intencion.alumno_boleta='".$boleta."')AS F1 inner join asignatura on asignatura.idmateria=F1.asignatura_idmateria)AS F2 inner join situacion on F2.situacion_idsituacion=situacion.idsituacion;";
+                $result2 = mysqli_query($conn2,$sql2);
+                $resultCheck2 = mysqli_num_rows($result2;
+                $tabla2 = [];
+                if($resultCheck2>0){
+                    while($row2 = mysqli_fetch_assoc($result2)){
+                        $tabla2[] = $row2;
+                    }
+                } else echo "Ninguno";
+                //echo json_encode($tabla);
+            }
+            echo json_encode($tabla2);
+        }
+        else{
+            echo 0;
+        }
+    }
 ?>
