026fa7d36e725df250eddb31643be8e579b33362
Scormit <sbenitezr@outlook.com>
2020-06-08 13:50:43 -0500

    registro14

--- src/alumno/materias/selectMaterias.php
+++ src/alumno/materias/selectMaterias.php
@@ -1,27 +1,27 @@
 <?php
-    //session_start();
+    session_start();
     if(isset($_POST)){
         echo "Entre a post";
-        $boleta = '2020630421';
+        $boleta = $_SESSION['user'];
         if(!empty($_POST['curses'])){
             $curses = $_POST['curses'];
-            echo "Obtengo curses";
+            echo " Obtengo curses";
         }
         if(!empty($_POST['recurses'])){
             $recurses = $_POST['recurses'];
-            echo "Obtengo recurses";
+            echo " Obtengo recurses";
         }
         require_once('./../../../config/mysqli_connect.php');
         $sql = "select fecha_intencion from intencion where alumno_boleta='".$boleta."';";
         $result = mysqli_query($conn,$sql);
         $resultCheck = mysqli_num_rows($result);
         if($resultCheck==0){
-            echo "No he metido materias activado";
+            echo " No he metido materias activado";
             $sql = "select count(*) from intencion;";
             $result = mysqli_query($conn,$sql);
             $resultCheck = mysqli_num_rows($result);
             if($resultCheck>0){
-                echo "Obtengo la cuenta";
+                echo " Obtengo la cuenta";
                 while($row = mysqli_fetch_assoc($result)){
                     $idIntencion = $row['count(*)'] + 1;
                 }
@@ -38,7 +38,7 @@
                         $result = mysqli_query($conn,$sql);
                         $resultCheck = mysqli_num_rows($result);
                         if($resultCheck>0){
-                            echo "Inserto curse";
+                            echo " Inserto curse";
                             while($row = mysqli_fetch_assoc($result)){
                                 $idMat = $row['idmateria'];
                             }
@@ -55,7 +55,7 @@
                         $result = mysqli_query($conn,$sql);
                         $resultCheck = mysqli_num_rows($result);
                         if($resultCheck>0){
-                            echo "Inserto recurse";
+                            echo " Inserto recurse";
                             while($row = mysqli_fetch_assoc($result)){
                                 $idMat = $row['idmateria'];
                             }
@@ -67,16 +67,16 @@
                 $sql = "SELECT F2.materia,situacion.situacion FROM (SELECT asignatura.materia,F1.situacion_idsituacion FROM (SELECT asignatura_intencion.asignatura_idmateria,asignatura_intencion.situacion_idsituacion FROM asignatura_intencion INNER JOIN intencion ON intencion.idintencion=asignatura_intencion.intencion_idintencion where intencion.alumno_boleta='".$boleta."')AS F1 inner join asignatura on asignatura.idmateria=F1.asignatura_idmateria)AS F2 inner join situacion on F2.situacion_idsituacion=situacion.idsituacion;";
                 $result = mysqli_query($conn,$sql);
                 $resultCheck = mysqli_num_rows($result);
-                //$tabla = [];
+                $tabla = [];
                 if($resultCheck>0){
-                    echo "Intencion FINAL";
+                    echo " Intencion FINAL";
                     while($row = mysqli_fetch_assoc($result)){
                         echo json_encode($row);
                     }
-                } else echo "Ninguno";
+                } else echo " Ninguno";
                 //echo json_encode($tabla);
             }
-            //echo json_encode($tabla);
+            echo json_encode($tabla);
         }
         else{
             echo 0;
